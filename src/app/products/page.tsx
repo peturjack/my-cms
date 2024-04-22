aa4c49523d685d3f57988434cda74dac140680a6
Petur jack <peturwj@gmail.com>
2024-04-22 14:21:30 +0000

    testing to return json data

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,14 +3,45 @@ import React, { Dispatch, SetStateAction } from "react";
 import Link from "next/link";
 
 const getData = async () => {
-  const res = await fetch("https://my-cms-alpha.vercel.app/products", {
-    cache: "no-cache",
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  //const res = await fetch("https://my-cms-alpha.vercel.app/api/products", {
+  // cache: "no-cache",
+  //});
+  //if (!res.ok) {
+  // throw new Error("Failed to fetch data");
+  // }
 
-  return res.json();
+  //return //res.json();
+  return {
+    allProducts: [
+      {
+        id: 2,
+        image:
+          "https://res.cloudinary.com/dtzca3rj5/image/upload/v1711634220/Car%20rental/cars/_a23bb3f8-81b6-44a8-9cb6-5d9d26977f17_wu1xqi.jpg",
+        title: "Nissan juke",
+        description:
+          "I am selling my nissan jeep so that i can have money for a new car",
+        owner_id: 6,
+      },
+      {
+        id: 4,
+        image:
+          "https://res.cloudinary.com/dtzca3rj5/image/upload/v1711633604/Car%20rental/cars/benjamin-voros-phIFdC6lA4E-unsplash_yobg57.jpg",
+        title: "Mountain hikes",
+        description: "Another crazy mountain hike for adventurers",
+        owner_id: 6,
+      },
+      {
+        id: 11,
+        image:
+          "https://res.cloudinary.com/dtzca3rj5/image/upload/v1711634859/Car%20rental/cars/ales-krivec-N-aTikX-b00-unsplash_o9yujq.jpg",
+        title: "Crazy hike",
+        description:
+          "I will take you on the hike of your dreams!! try to keep up with me hahahah",
+        owner_id: 8,
+      },
+    ],
+    status: 200,
+  };
 };
 
 const Products = async () => {
